c9b566d411caa0e8e02e0c85f4fb468e1f358a5c
student <student@gnu.ac.in>
2023-07-18 09:34:40 +0530

    practical1.1

---
+++ src/main/kotlin/practical1.1.kt
@@ -0,0 +1,25 @@
+fun main(){
+var a:Int=22
+var b:Float=1.5f
+var c:Char='N'
+var d:String="Naitri"
+var e:Boolean=false
+var f:Double=96.56
+var g:Long=32245435
+var h:Short=-3
+var i:Byte=123
+
+    println("Int value : $a")
+    println("Float value :$b")
+    println("Char value:$c")
+    println("String value:$d")
+    println("Boolean value:$e")
+    println("Double value:$f")
+    println("Long value:$g")
+    println("Short value:$h")
+    println("Byte value:$i")
+
+
+
+
+}
