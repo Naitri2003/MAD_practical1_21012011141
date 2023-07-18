eb88a483ff357952dd9477da348d236862ffe4fe
student <student@gnu.ac.in>
2023-07-18 10:40:00 +0530

    practical1

---
+++ src/main/kotlin/practical1.6.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var a:Long=100
+    var b:Long=50
+    var c:Long=40
+
+    var result1:Long=a+b+c
+    println("Addition of $a,$b and $c is :$result1")
+
+    var result2:Long=a-b-c
+    println("Subtraction of $a ,$b and $c is :$result2")
+
+    var result3:Long=a*b*c
+    println("Multiplication of $a, $b and $c is :$result3")
+
+    var result4:Long=a/b
+    println("Division of $a ,$b is: $result4")
+}
