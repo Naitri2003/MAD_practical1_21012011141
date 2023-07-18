be6fa954ba4e66349c0c34e72e871c0ce1d7c354
student <student@gnu.ac.in>
2023-07-18 10:06:32 +0530

    practical1.3

---
+++ src/main/kotlin/practical1.3.kt
@@ -0,0 +1,29 @@
+fun main(){
+
+    println("Student Enrollment number :")
+    val eno= readLine()!!
+    println("Student Name: ")
+    val name= readLine()!!
+    println("Student branch: ")
+    val branch= readLine()!!
+    println("Student class: ")
+    val cls= readLine()!!
+    println("Student Batch:")
+    val batch= readLine()!!
+    println("Student college name:")
+    val clg= readLine()!!
+    println("University name :")
+    val univ= readLine()!!
+    println("Student age:")
+    var age= readLine()!!
+
+    println("Student's data")
+    println("Enrollment number:$eno")
+    println("Name:$name")
+    println("Branch:$branch")
+    println("Class:$cls")
+    println("Batch :$batch")
+    println("collage:$clg")
+    println("University:$univ ")
+    println("Age:$age")
+}
