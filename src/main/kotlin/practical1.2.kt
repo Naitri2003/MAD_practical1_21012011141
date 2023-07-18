f6862a67773bff58e22a04566b65e800d5474239
student <student@gnu.ac.in>
2023-07-18 09:44:25 +0530

    practical1.1

---
+++ src/main/kotlin/practical1.2.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var a:Int =10
+    println("Integer value :$a")
+    println("Double value (from integer):${a.toDouble()}")
+    var b:String="10"
+    println("String value : $b")
+    println("Integer value (from string):${b.toInt()}")
+    println("Double value(From String):${b.toDouble()}")
+}
