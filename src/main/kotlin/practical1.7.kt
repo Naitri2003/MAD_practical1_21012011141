3be1b95522f203bdaa632bf48118366c3d8e3512
Naitri2003 <132800074+Naitri2003@users.noreply.github.com>
2023-07-27 21:59:05 +0530

    practical1

---
+++ src/main/kotlin/practical1.7.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val number=5
+    val result = factorial(number)
+    print("Enter a Number:")
+    val number1 = readLine()!!
+    println("Factorial of $number is: $result")
+    println(" By TailRec Keyword ,Factorial of $number is:$result")
+
+}
+
+fun factorial(n: Int): Long {
+    return if (n == 0) {
+        1
+    } else {
+        n * factorial(n - 1)}}
